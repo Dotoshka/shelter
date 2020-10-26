737fe851cdff08f02f25a370f346e60a2e74b486
Tamara <yaroslavcevatamara@gmail.com>
2020-10-26 04:55:07 +0300

    feat: add pagination for pets page

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,5 +1,4 @@
 /* Overlay */
-
 const body = document.querySelector('body');
 const header = document.querySelector('header');
 const overlay = document.getElementById('overlay');
@@ -10,22 +9,30 @@ overlay.addEventListener('click', () => {
         closeModal(popup);
     })
     if (openMenu === true) {
-        hideMenu();    
+        hideMenu();
     }
 })
 
-/* Burger-menu */
+/* Buttons */
+const mainPageButtons = document.querySelectorAll('[data-main-page]');
+mainPageButtons.forEach(button => { button.addEventListener('click', () => document.location = '../main/main.html'); })
 
+//Random
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+/* Burger-menu */
 let menuButton = document.getElementById('burger-menu');
 let menuWindow = document.getElementById('menu-window');
 let openMenu = false;
 
 menuButton.addEventListener('click', () => {
-   
+
     if (openMenu === false) {
-        showMenu(); 
+        showMenu();
     } else {
-        hideMenu();   
+        hideMenu();
     }
 });
 
@@ -42,151 +49,253 @@ const showMenu = () => {
 }
 
 const hideMenu = () => {
-    
+
     body.style.overflow = 'initial';
-    header.style.position = 'fixed'; 
-    overlay.classList.remove('active'); 
+    header.style.position = 'fixed';
+    overlay.classList.remove('active');
     menuWindow.classList.remove('active', 'main');
     logo.classList.remove('active', 'main');
     menuButton.classList.remove('active');
-    openMenu = false;   
+    openMenu = false;
 }
 
-
-/* Buttons */
-
-//const petsPageButtons = document.querySelectorAll('[data-pets-page]');
-const mainPageButtons = document.querySelectorAll('[data-main-page]');
-
-//petsPageButtons.forEach(button => { button.addEventListener('click', () => document.location = '../pets/pets.html'); })
-mainPageButtons.forEach(button => { button.addEventListener('click', () => document.location = '../main/main.html'); })
-
-// 
-
+// Pagination
 const sliderCards = document.querySelector('[data-slider-cards]');
+let pets = [];
+let fullPetsList = [];
+let currentPage = 1;
+let itemsPerPage = 8;
 
-const pets = [
-    {
-      "name": "Jennifer",
-      "img": "../../assets/images/pets/pets-jennifer.png",
-      "type": "Dog",
-      "breed": "Labrador",
-      "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
-      "age": "2 months",
-      "inoculations": ["none"],
-      "diseases": ["none"],
-      "parasites": ["none"]
-    },
-    {
-      "name": "Sophia",
-      "img": "../../assets/images/pets/pets-sophia.png",
-      "type": "Dog",
-      "breed": "Shih tzu",
-      "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
-      "age": "1 month",
-      "inoculations": ["parvovirus"],
-      "diseases": ["none"],
-      "parasites": ["none"]
-    },
-    {
-      "name": "Woody",
-      "img": "../../assets/images/pets/pets-woody.png",
-      "type": "Dog",
-      "breed": "Golden Retriever",
-      "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
-      "age": "3 years 6 months",
-      "inoculations": ["adenovirus", "distemper"],
-      "diseases": ["right back leg mobility reduced"],
-      "parasites": ["none"]
-    },
-    {
-      "name": "Scarlett",
-      "img": "../../assets/images/pets/pets-scarlett.png",
-      "type": "Dog",
-      "breed": "Jack Russell Terrier",
-      "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
-      "age": "3 months",
-      "inoculations": ["parainfluenza"],
-      "diseases": ["none"],
-      "parasites": ["none"]
-    },
-    {
-      "name": "Katrine",
-      "img": "../../assets/images/pets/pets-katrine.png",
-      "type": "Cat",
-      "breed": "British Shorthair",
-      "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
-      "age": "6 months",
-      "inoculations": ["panleukopenia"],
-      "diseases": ["none"],
-      "parasites": ["none"]
-    },
-    {
-      "name": "Timmy",
-      "img": "../../assets/images/pets/pets-timmy.png",
-      "type": "Cat",
-      "breed": "British Shorthair",
-      "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
-      "age": "2 years 3 months",
-      "inoculations": ["calicivirus", "viral rhinotracheitis"],
-      "diseases": ["kidney stones"],
-      "parasites": ["none"]
-    },
-    {
-      "name": "Freddie",
-      "img": "../../assets/images/pets/pets-freddie.png",
-      "type": "Cat",
-      "breed": "British Shorthair",
-      "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
-      "age": "2 months",
-      "inoculations": ["rabies"],
-      "diseases": ["none"],
-      "parasites": ["none"]
-    },
-    {
-      "name": "Charly",
-      "img": "../../assets/images/pets/pets-charly.png",
-      "type": "Dog",
-      "breed": "Jack Russell Terrier",
-      "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
-      "age": "8 years",
-      "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
-      "diseases": ["deafness", "blindness"],
-      "parasites": ["lice", "fleas"]
+const checkItemsPerPage = () => {
+    if (body.offsetWidth > 768 && body.offsetWidth < 1280) {
+        itemsPerPage = 6;
+    } else if (body.offsetWidth < 768) {
+        itemsPerPage = 3;
     }
-  ];
-
-const getRandomInt = (max) => {
-    return Math.floor(Math.random() * Math.floor(max));
 }
 
-const createCards = () => {
-    for (i = 0; i < 8; i++) {
-        let counter = getRandomInt(7);
-        //let counter = 0;
-        sliderCards.insertAdjacentHTML('beforeend', 
-        `<div class="card-container">
-            <div class="slider-card" data-modal-target="#${pets[counter].name}">
-                <img class="slider-card__img" src=${pets[counter].img} alt=${pets[counter].name}>
-                <p class="slider-card__title">${pets[counter].name}</p>
-                <button class="slider-card__button">Learn more</button>
-            </div>
-        </div>`
-        );
+window.addEventListener(`resize`, () => {
+    currentPage = 1;
+    curPageBtn.innerHTML = currentPage;
+    prevPageBtn.setAttribute('disabled', 'disabled');
+    firstPageBtn.setAttribute('disabled', 'disabled');
+    nextPageBtn.removeAttribute('disabled', 'disabled');
+    lastPageBtn.removeAttribute('disabled', 'disabled');
+    itemsPerPage = 8;
+    checkItemsPerPage();
+    changeImgSet(currentPage, itemsPerPage);
+});
 
-        body.insertAdjacentHTML('beforeend', 
-        `<div class="popup" id=${pets[counter].name}>
+fetch('../../assets/pets.json')
+    .then(res => res.json())
+    .then(data => {
+        pets = data;
+        fullPetsList = (() => {
+            let tempArr = [];
+
+            for (let i = 0; i < 6; i++) {
+                const newPets = pets;
+
+                for (let j = pets.length; j > 0; j--) {
+                    let randInd = Math.floor(Math.random() * j);
+                    const randElem = newPets.splice(randInd, 1)[0];
+                    newPets.push(randElem);
+                }
+
+                tempArr = [...tempArr, ...newPets];
+            }
+            return tempArr;
+        })();
+
+        fullPetsList = sort863(fullPetsList);
+
+        createPets(fullPetsList);
+        createPopups();
+        checkItemsPerPage();
+        changeImgSet(currentPage, itemsPerPage);
+        //document.querySelector("#currentPage").innerText = (currentPage + 1).toString();
+    })
+
+const createPets = (petsList) => {
+    sliderCards.innerHTML += createElements(petsList);
+}
+
+createElements = (petsList) => {
+    let str = '';
+    for (let i = 0; i < petsList.length; i++) {
+        str += `<div class="card-container">
+                    <div class="slider-card" data-modal-target="#${petsList[i].name}">
+                        <img class="slider-card__img" src=${petsList[i].img} alt=${petsList[i].name}>
+                        <p class="slider-card__title">${petsList[i].name}</p>
+                        <button class="slider-card__button">Learn more</button>
+                    </div>
+                </div>`;
+    }
+
+    return str;
+}
+
+const sort863 = (list) => {
+    let unique8List = [];
+    let length = list.length;
+    for (let i = 0; i < length / 8; i++) {
+        const uniqueStepList = [];
+        for (j = 0; j < list.length; j++) {
+            if (uniqueStepList.length >= 8) {
+                break;
+            }
+            const isUnique = !uniqueStepList.some((item) => {
+                return item.name === list[j].name;
+            });
+            if (isUnique) {
+                uniqueStepList.push(list[j]);
+                list.splice(j, 1);
+                j--;
+            }
+        }
+        unique8List = [...unique8List, ...uniqueStepList];
+    }
+    list = unique8List;
+
+    list = sort6recursively(list);
+    return list;
+}
+
+const sort6recursively = (list) => {
+    const length = list.length;
+
+    for (let i = 0; i < (length / 6); i++) {
+        const stepList = list.slice(i * 6, (i * 6) + 6);
+
+        for (let j = 0; j < 6; j++) {
+            const duplicatedItem = stepList.find((item, ind) => {
+                return item.name === stepList[j].name && (ind !== j);
+            });
+
+            if (duplicatedItem !== undefined) {
+                const ind = (i * 6) + j;
+                const which8OfList = Math.trunc(ind / 8);
+
+                list.splice(which8OfList * 8, 0, list.splice(ind, 1)[0]);
+
+                sort6recursively(list);
+            }
+        }
+    }
+    return list;
+}
+
+const firstPageBtn = document.querySelector('.first-page');
+const prevPageBtn = document.querySelector('.previous-page');
+const nextPageBtn = document.querySelector('.next-page');
+const lastPageBtn = document.querySelector('.last-page');
+const curPageBtn = document.querySelector('.current-page');
+curPageBtn.innerHTML = currentPage;
+
+const changeImgSet = (currentPage, itemsPerPage) => {
+    let sliderItems = sliderCards.children;
+    let startAdd = (currentPage - 1) * itemsPerPage;
+    let endAdd = (currentPage - 1) * itemsPerPage + itemsPerPage;
+    
+    for (let j = 0; j < sliderItems.length; j++) {
+        sliderItems[j].classList.remove('active');
+    }
+    for (let i = startAdd; i < endAdd; i++) {
+        sliderItems[i].classList.add('active');
+    }
+}
+
+nextPageBtn.addEventListener('click', () => {
+    checkItemsPerPage();
+    let pagesCount = fullPetsList.length / itemsPerPage;
+    console.log(`pagesCount: ${pagesCount}`);
+    if (currentPage < pagesCount) {
+        currentPage++;
+        changeImgSet(currentPage, itemsPerPage)
+        prevPageBtn.removeAttribute('disabled');
+        firstPageBtn.removeAttribute('disabled');
+        console.log(`currentPage: ${currentPage}`);
+        curPageBtn.innerHTML = currentPage;
+        if (currentPage === pagesCount) {
+            nextPageBtn.setAttribute('disabled', 'disabled');
+            lastPageBtn.setAttribute('disabled', 'disabled');
+        }
+    }
+})
+
+prevPageBtn.addEventListener('click', () => {
+    checkItemsPerPage();
+    let pagesCount = fullPetsList.length / itemsPerPage;
+    console.log(`pagesCount: ${pagesCount}`);
+    if (currentPage > 1) {
+        currentPage--;
+        changeImgSet(currentPage, itemsPerPage)
+        nextPageBtn.removeAttribute('disabled');
+        lastPageBtn.removeAttribute('disabled');
+        console.log(`currentPage: ${currentPage}`);
+        curPageBtn.innerHTML = currentPage;
+        if (currentPage === 1) {
+            prevPageBtn.setAttribute('disabled', 'disabled');
+            firstPageBtn.setAttribute('disabled', 'disabled');
+        }
+    }
+})
+
+lastPageBtn.addEventListener('click', () => {
+    checkItemsPerPage();
+    let pagesCount = fullPetsList.length / itemsPerPage;
+    console.log(`pagesCount: ${pagesCount}`);
+    if (currentPage < pagesCount) {
+        currentPage = pagesCount;
+        changeImgSet(currentPage, itemsPerPage);
+        prevPageBtn.removeAttribute('disabled');
+        firstPageBtn.removeAttribute('disabled');
+        console.log(`currentPage: ${currentPage}`);
+        curPageBtn.innerHTML = currentPage;
+        if (currentPage === pagesCount) {
+            nextPageBtn.setAttribute('disabled', 'disabled');
+            lastPageBtn.setAttribute('disabled', 'disabled');
+        }
+    }
+})
+
+firstPageBtn.addEventListener('click', () => {
+    checkItemsPerPage();
+    let pagesCount = fullPetsList.length / itemsPerPage;
+    console.log(`pagesCount: ${pagesCount}`);
+    if (currentPage > 1) {
+        currentPage = 1;
+        changeImgSet(currentPage, itemsPerPage);
+        nextPageBtn.removeAttribute('disabled');
+        lastPageBtn.removeAttribute('disabled');
+        console.log(`currentPage: ${currentPage}`);
+        curPageBtn.innerHTML = currentPage;
+        if (currentPage === 1) {
+            prevPageBtn.setAttribute('disabled', 'disabled');
+            firstPageBtn.setAttribute('disabled', 'disabled');
+        }
+    }
+})
+
+/* Popup */
+const popupsContainer = document.querySelector('.popups-container');
+
+const createPopups = () => {
+    for (i = 0; i < pets.length; i++) {
+
+        popupsContainer.insertAdjacentHTML('beforeend',
+            `<div class="popup" id=${pets[i].name}>
             <button class="close-button" data-close-button><img src=../../assets/icons/icon-close.svg alt="Close"></button>
-            <div class="popup__img"><img src=${pets[counter].img} alt=${pets[counter].name} class="content__img"></div>
+            <div class="popup__img"><img src=${pets[i].img} alt=${pets[i].name} class="content__img"></div>
             <div class="popup-content">
-                <p class="popup-content__title">${pets[counter].name}</p>
-                <p class="popup-content__subtitle">${pets[counter].type} - ${pets[counter].breed}</p>
-                <p class="popup-content__info">${pets[counter].description}</p>
+                <p class="popup-content__title">${pets[i].name}</p>
+                <p class="popup-content__subtitle">${pets[i].type} - ${pets[i].breed}</p>
+                <p class="popup-content__info">${pets[i].description}</p>
                 <ul class="popup-content__ul">
-                        <li><strong>Age: </strong>${pets[counter].age}</li>
-                        <li><strong>Inoculations: </strong>${pets[counter].inoculations}</li>
-                        <li><strong>Diseases: </strong>${pets[counter].diseases}</li>
-                        <li><strong>Parasites: </strong>${pets[counter].parasites}</li>
+                        <li><strong>Age: </strong>${pets[i].age}</li>
+                        <li><strong>Inoculations: </strong>${pets[i].inoculations}</li>
+                        <li><strong>Diseases: </strong>${pets[i].diseases}</li>
+                        <li><strong>Parasites: </strong>${pets[i].parasites}</li>
                 </ul>
             </div>   
         </div>`
@@ -194,36 +303,33 @@ const createCards = () => {
     }
 }
 
-
-createCards();
-
-/* Popup */
-
-const openModalButtons = document.querySelectorAll('[data-modal-target]');
-const closeModalButtons = document.querySelectorAll('[data-close-button]');
-
-openModalButtons.forEach(button => { 
-    button.addEventListener('click', () => {
-        const popup = document.querySelector(button.dataset.modalTarget);
+sliderCards.addEventListener('click', function (e) {
+    let cardContainer = e.target.classList.contains('card-container');
+    if (e.target !== this && !cardContainer && this.contains(e.target)) {
+        const popupId = e.target.closest('[data-modal-target]').dataset.modalTarget
+        const popup = document.querySelector(popupId);
         openModal(popup);
-    }); 
-})
+    }
+});
 
-closeModalButtons.forEach(button => { 
-    button.addEventListener('click', () => {
-        const popup = button.closest('.popup')
+popupsContainer.addEventListener('click', function (e) {
+    const closeButton = e.target.closest('[data-close-button]');
+    if (e.target !== this && closeButton) {
+        const popup = closeButton.closest('.popup');
         closeModal(popup);
-    }); 
-})
+    }
+});
 
 function openModal(popup) {
     if (popup == null) return
     popup.classList.add('active');
     overlay.classList.add('active');
+    body.style.overflow = 'hidden';
 }
 
 function closeModal(popup) {
     if (popup == null) return
     popup.classList.remove('active');
     overlay.classList.remove('active');
+    body.style.overflow = 'initial';
 }
